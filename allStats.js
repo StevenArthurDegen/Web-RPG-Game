eb9d566212efbcdf667c0fa13a9679832d364ba2
Steven Arthur <steven.arthur.degen@gmail.com>
2023-12-24 02:34:30 -0500

    added allStats.js
    this is a module that holds all monsters objects in an array with all info about monster,
    also holds player and their info

---
+++ allStats.js
@@ -0,0 +1,35 @@
+const player = {
+    health: 100,
+    level: 1,
+    exp: 0,
+    alive: true, 
+    "attack power": 10
+}
+const monsters = [
+    {
+        name: 'Low level Goblin 1',
+        health: 100,
+        level: 1,
+        exp: 5,
+        alive: true,
+        "attack power": 10
+    },
+    {
+        name: 'Low level Goblin 2',
+        health: 200,
+        level: 1,
+        exp: 5,
+        alive: true,
+        "attack power": 10
+    },
+    {
+        name: 'Low level Goblin 3',
+        health: 300,
+        level: 1,
+        exp: 5,
+        alive: true,
+        "attack power": 10
+    },
+];
+
+export {player, monsters}
